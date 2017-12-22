a50fb247bc4d418135a766c62b8dbfd789fbd454
lixiaoxun <open_source@126.com>
2017-12-22 17:21:43 +0800

    测试

--- test.py
+++ test.py
@@ -1,33 +1,19 @@
-page = """
-<h1>this is a test</h1>
-<ul>
-    {% for title in titles %}
-        <li>{{ title | upper | local }}</li>
-    {% endfor %}
-</ul>
-{% if shown %}
-    <div>show something</div>
-{% endif %}
-"""
-
-
 from monk import Monk, CodeBuilder
 
-# # m = Monk('test')
-# code = CodeBuilder()
+def test():
+    page = """
+    <h1>this is a test</h1>
+    <ul>
+        {% for title in titles %}
+            <li>{{ title | upper | local }}</li>
+        {% endfor %}
+    </ul>
+    {% if shown %}
+        <div>show something</div>
+    {% endif %}
+    """
 
-# buffered = []
+    m = Monk(page)
 
-
-# def flush_output():
-#     if len(buffered) == 1:
-#         code.add_line('append_result(%s)' % buffered[0])
-#     elif len(buffered) > 1:
-#         code.add_line('extend_result([%s])' % ''.join(buffered))
-#     del buffered[:]
-
-
-# buffered.append([1, 2, 3, 4, 5])
-# flush_output()
-# print(code)
-m = Monk(page)
+if __name__ == '__main__':
+    test()
