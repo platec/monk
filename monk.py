7ca6a546ebe8908163e40f61c9fda8a28dbcb00b
lixiaoxun <open_source@126.com>
2017-12-26 10:51:33 +0800

    使用分词器

--- monk.py
+++ monk.py
@@ -1,8 +1,9 @@
 import re
-INDENT_STEP = 4
 
 
 class CodeBuilder:
+    INDENT_STEP = 4
+
     def __init__(self, indent=0):
         self.code = []
         self.indent_level = indent
@@ -11,10 +12,10 @@ class CodeBuilder:
         self.code.extend([' ' * self.indent_level + line + '\n'])
 
     def indent(self):
-        self.indent_level += INDENT_STEP
+        self.indent_level += self.INDENT_STEP
 
     def dedent(self):
-        self.indent_level -= INDENT_STEP
+        self.indent_level -= self.INDENT_STEP
 
     def add_section(self):
         section = CodeBuilder(self.indent_level)
@@ -25,7 +26,7 @@ class CodeBuilder:
         return ''.join([str(c) for c in self.code])
 
     def get_globals(self):
-        # assert self.indent_level == 0
+        assert self.indent_level == 0
         python_source = str(self)
         global_namespaces = {}
         exec(python_source, global_namespaces)
@@ -50,83 +51,33 @@ class Monk:
         code.add_line('extend_result = result.extend')
         code.add_line('to_str = str')
 
-        buffered = []
-
-        def flush_output():
-            if len(buffered) == 1:
-                code.add_line('append_result(%s)' % buffered[0])
-            elif len(buffered) > 1:
-                code.add_line('extend_result([%s])' % ','.join(buffered))
-            del buffered[:]
-
-        template_settings = {
-            'expression': r'{{.*?}}',  # 表达式
-            'logic': r'{%.*?%}',  # 逻辑控制
-            'comment': r'{#.*?#}'  # 注释
-        }
-
-        pattern = re.compile('(' + '|'.join(template_settings[key] for key in template_settings) + ')')
-        tokens = re.split(pattern, text)
-        print(tokens)
+        tokens = self._generate_tokens(text)
         for token in tokens:
-            if token.startswith('{#'):
+            name = token[0]
+            expr = token[1].strip()
+            if name == 'html':
+                code.add_line('append_result("""%s""")' % expr)
+            elif name == 'expression':
+                code.add_line("c_%s = context['%s']" %(expr, expr))
+                code.add_line("append_result(to_str(c_%s))" % expr)
+            elif name == 'comment':
                 continue
-            if token.startswith('{{'):
-                expr = self._expr_code(token[2:-2].strip())
-                buffered.append('to_str(%s)' % expr)
-                self.all_vars.add(expr)
-            elif token.startswith('{%'):
-                flush_output()
-                words = token[2:-2].strip().split()
-    
-        self._render_function = code.get_globals()['render_function']
-
-    def _expr_code(self, expr):
-        if '|' in expr:
-            pipes = expr.split('|')
-            code = self._expr_code(pipes[0])
-            for func in pipes[1:]:
-                func = func.strip()
-                self._variable(func, self.all_vars)
-                code = 'c_%s(%s)' % (func, code)
-        elif '.' in expr:
-            dots = expr.split('.')
-            code = self._expr_code(dots[0])
-            args = ', '.join(repr(d) for d in dots[1:])
-            code = 'do_dots(%s, %s)' % (code, args)
-        else:
-            self._variable(expr, self.all_vars)
-            code = 'c_%s' % expr
-        return code
-
-    def _do_dots(self, value, *dots):
-        for dot in dots:
-            # try:
-            #     value = getattr(value, dot, '')
-            # except AttributeError:
-            #     value = value[dot]
-            if callable(value):
-                value = value()
             else:
-                value = getattr(value, dot, '')
-        return value
-    
-    def _syntax_error(self, msg, thing):
-        raise MonkException('%s: %r' % (msg, thing))
+                pass
+        code.add_line("return ''.join(result)")
+        code.dedent()
+        print(code.get_globals()['render_function'](self.context, None))
+        print(code)
 
-
-    def _variable(self, name, vars_set):
-        name = name.strip()
-        if not re.match(r'[_a-zA-Z][_a-zA-Z0-9]*$', name):
-            self._syntax_error('Not a valid name', name)
-        vars_set.add(name)
-
-    def render(self, context=None):
-        render_context = dict(self.context)
-        if context:
-            render_context.update(context)
-        return self._render_function(render_context, self._do_dots)
-
-
-class MonkException(Exception):
-    pass
+    def _generate_tokens(self, text):
+        pattern = re.compile(
+            r'{{(?P<expression>.*?)}}|{%(?P<logic>.*?)%}|{#(?P<comment>.*?)#}')
+        matches = pattern.finditer(text)
+        start = 0
+        end = 0
+        for m in matches:
+            end = m.span()[0]
+            yield 'html', text[start: end]
+            start = m.span()[1]
+            yield m.lastgroup, m.group(m.lastgroup)
+        yield 'html', text[start:]
